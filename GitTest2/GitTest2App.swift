dcf38eb60c066cf25e1a3ee07f03eb740b3caa2e
ryumbrella <sani9511@naver.com>
2024-04-18 20:17:01 +0900

    Initial Commit

---
+++ GitTest2/GitTest2App.swift
@@ -0,0 +1,17 @@
+//
+//  GitTest2App.swift
+//  GitTest2
+//
+//  Created by  SAN RYU on 4/18/24.
+//
+
+import SwiftUI
+
+@main
+struct GitTest2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
