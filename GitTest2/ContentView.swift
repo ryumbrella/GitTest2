e510981b5fd41ba888dd7f467801b0a8d7df8648
ryumbrella <sani9511@naver.com>
2024-04-18 20:40:32 +0900

    자운드 안녕! 두 번째 안녕!

--- GitTest2/ContentView.swift
+++ GitTest2/ContentView.swift
@@ -10,9 +10,10 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
+            Image(systemName: "heart")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
+            Text("Hello, JJJJound!")
             Text("Hello, JJJJound!")
         }
         .padding()
